b7929c6b11ebf703616adfc76d45f45e7037465f
swarnav.das <sdas680.sd@gmail.com>
2024-01-18 14:56:53 +0530

    producer-consumer problem

---
+++ src/main/java/org/example/producerConsumer/Main.java
@@ -0,0 +1,17 @@
+package org.example.producerConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Resource resource = new Resource(5);
+
+        Producer producer = new Producer(resource);
+        Consumer consumer = new Consumer(resource);
+
+
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
---
+++ src/main/java/org/example/producerConsumer/Resource.java
@@ -0,0 +1,43 @@
+package org.example.producerConsumer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Resource {
+
+    Queue<Integer> data;
+    private Integer bufferSize;
+
+    public Resource(Integer bufferSize) {
+        this.bufferSize = bufferSize;
+        this.data = new LinkedList<>();
+    }
+
+    public synchronized void addItem(Integer item) {
+        if (data.size() == bufferSize) {
+            try {
+                wait();
+            } catch (Exception e) {
+                System.out.println("Exception during thread wait" + Thread.currentThread().getName());
+            }
+        }
+
+        data.add(item);
+        System.out.println("Produced Data. Item: " + item);
+        notifyAll();
+    }
+
+    public synchronized void consumeItem() {
+        while (data.size() == 0) {
+            try {
+                wait();
+            } catch (Exception e) {
+                System.out.println("Exception during thread wait" + Thread.currentThread().getName());
+            }
+        }
+
+        Integer item = data.poll();
+        System.out.println("Consumed data.Item: " + item);
+        notifyAll();
+    }
+}
